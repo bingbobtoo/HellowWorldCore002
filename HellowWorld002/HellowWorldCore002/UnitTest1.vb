5224927c9872b33a115b3ea234593ceab0f7807c
bingobobtoo <x@x.com>
2020-04-18 19:30:13 +0100

    init

---
+++ HellowWorld002/HellowWorldCore002/UnitTest1.vb
@@ -0,0 +1,14 @@
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Namespace HellowWorldCore002
+    <TestClass>
+    Public Class UnitTest1
+        <TestMethod>
+        Sub TestSub()
+
+            Assert.AreEqual(1, 1)
+
+        End Sub
+    End Class
+End Namespace
+
